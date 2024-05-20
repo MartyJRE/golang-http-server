4e350e2f3b89392691a65e95fddf8247ec3eacb0
Martin Pražák <martin.prazak@lutherone.com>
2024-05-20 20:39:39 +0200

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
+    "log"
     "net"
     "os"
+    "strings"
 )
 
 func main() {
@@ -20,9 +22,45 @@ func main() {
         os.Exit(1)
     }
 
-    _, err = connection.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+    buffer := make([]byte, 1024)
+    _, err = connection.Read(buffer)
     if err != nil {
-        fmt.Println("")
+        fmt.Println("Error reading data: ", err.Error())
         os.Exit(1)
+    }
+
+    message := string(buffer)
+    splitLines := strings.Split(message, "\r\n")
+
+    requestLine := splitLines[0]
+    headers := splitLines[1 : len(splitLines)-1]
+    body := splitLines[len(splitLines)-1]
+
+    splitRequestLine := strings.Split(requestLine, string(' '))
+    method := splitRequestLine[0]
+    path := splitRequestLine[1]
+    protocol := splitRequestLine[2]
+
+    log.Println(fmt.Sprintf("[%s] %s - %s\n%s\n%v", protocol, method, path, headers, body))
+
+    switch path {
+    case "/":
+        {
+            writerBuffer := []byte("HTTP/1.1 200 OK\r\n\r\n")
+            _, err = connection.Write(writerBuffer)
+            if err != nil {
+                fmt.Println("Failed to write response: ", err.Error())
+                os.Exit(1)
+            }
+        }
+    default:
+        {
+            writerBuffer := []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+            _, err = connection.Write(writerBuffer)
+            if err != nil {
+                fmt.Println("Failed to write response: ", err.Error())
+                os.Exit(1)
+            }
+        }
     }
 }
